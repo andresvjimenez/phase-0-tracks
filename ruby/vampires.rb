125f8b4a00268852b3f5202bb292941fa5cab750
Andres Jimenez <andresvjimenez@icloud.com>
2016-12-20 14:27:12 -0600

    Submits 5.1 Using Ruby Documentation

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,73 +1,93 @@
-counter = 0
-year = 2016.to_i
-
-
-
-
-
-
-
-
 
 puts "What's your name"
 employee_name = gets.chomp
+ if employee_name == "Drake Cula" || "Tu Fang"
+  employee_name == false
+else
+  employee_name == true 
+end
 
 
 puts "How old are you?" 
 age = gets.to_i
-
 puts "What year were you born?"
-dob = gets.to_i
-
-if age == year - dob
-  age = true
-else
-  age = false
+dob = gets.to_i 
+year = 2016
+age_true = (year.to_i - dob.to_i)
+if age == age_true
+age == true
+else 
+  age == false
 end
 
 
 puts "Our company cafeteria serves garlic bread. 
 Should we order some for you? (y/n)"
 garlic = gets.chomp
-
 if garlic == "y"
-  garlic = true
-elsif garlic == "n"
-  garlic = false
+garlic == true
+else 
+  garlic == false
 end
 
 
 puts "Would you like to enroll in the company’s 
 health insurance? (y/n)"
 insurance = gets.chomp
-
 if insurance == "y"
-  insurance = true
-elsif insurance == "n"
-  insurance = false
+insurance == true 
+else 
+  insurance == false
 end
 
-case 
-
-   
-when age && (garlic || insurance) 
-  puts "Probably NOT a vampire."  
-
-when age && (garlic || insurance) 
-  puts "Probably a vampire."
-
-when age && (garlic && insurance) 
-  puts "Almost certainly a vampire."
-
-when employee_name == "Drake Cula" 
-  puts "Definitely a vampire."
-
-when employee_name == "Tu Fang"
-  puts "Definitely a vampire."
-else
-  puts "Results inconclusive."
 
 
-end
+
+
+
+
+
+ 
+
+
+ if (!garlic || insurance) && age && employee_name
+  p "Probably NOT a vampire."   
+
+ elsif (garlic || !insurance) && age && employee_name
+  p "Probably NOT a vampire."  
+
+
+ elsif (!garlic || insurance) && !age && employee_name
+  p "Probably a vampire."
+
+  elsif (garlic || !insurance) && !age && employee_name
+  p "Probably a vampire."
+
+
+ 
+ elsif (!garlic && !insurance) && !age && employee_name
+  p "Almost certainly a vampire."
+
+elsif (garlic || insurance) && age && !employee_name 
+  p "Definitely a vampire."
+
+ else
+  p "Results inconclusive."
+ end
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
 
 
