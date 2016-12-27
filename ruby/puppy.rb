e9d687d549e05694e20e0e12b9dc6920683f2936
Andres Jimenez <andresvjimenez@icloud.com>
2016-12-27 15:30:48 -0700

    completes 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,23 @@
+=begin
+
+
+SPECIES ------------------------
+Puppus Adorableus
+
+CHARACTERISTICS ----------------
+Eyelash length: varies
+Friendly: yes
+Eye count: 2
+Name: varies
+Color: varies
+Tale length: varies
+
+BEHAVIOR -----------------------
+Jump
+Hug
+Lick
+Bark
+
+
+  
+=end
